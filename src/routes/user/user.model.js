975bbde13ace3c0cfb549d013377fa265024bd88
jjikky <kok5417@skuniv.ac.kr>
2024-06-17 02:14:34 +0900

    🗃️ 유저스키마 정의

---
+++ src/routes/user/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+        deletedAt: { type: Date, default: null },
+        nickname: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String },
+        role: { type: String, default: 'user' },
+        snsId: { type: String, default: null },
+        provider: { type: String, default: null },
+    },
+    {
+        timestamps: { currentTime: () => Date.now() },
+    }
+);
+
+const model = mongoose.model('User', userSchema);
+
+module.exports = model;
