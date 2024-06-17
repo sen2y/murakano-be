79454082b4afcc1774250b5ee3f8a52e9d500395
jjikky <kok5417@skuniv.ac.kr>
2024-06-17 21:16:32 +0900

    🎨 성공 메세지 상수 파일 분리

---
+++ src/common/constants/success-message.js
@@ -0,0 +1,6 @@
+const SucesssMessage = Object.freeze({
+    // USER
+    REGISTER_SUCCESSS: 'register_sucess',
+});
+
+module.exports = SucesssMessage;
--- src/routes/user/user.controller.js
+++ src/routes/user/user.controller.js
@@ -1,14 +1,18 @@
 const userService = require('./user.service');
 const sendResponse = require('../../common/utils/response-handler');
 const ErrorMessage = require('../../common/constants/error-message');
+const SucesssMessage = require('../../common/constants/success-message');
 
 exports.register = async (req, res) => {
     try {
         const newUser = await userService.register(req.body);
-        console.log(newUser);
-        sendResponse.created(res, {
-            message: 'user registered successfully',
+
+        data = {
             user_id: newUser._id,
+        };
+        sendResponse.created(res, {
+            message: SucesssMessage.REGISTER_SUCCESSS,
+            data,
         });
     } catch (err) {
         sendResponse.fail(req, res, ErrorMessage.REGISTER_ERROR);
