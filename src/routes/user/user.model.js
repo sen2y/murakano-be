f691d5511c3517f5ee5d110664e27d69b97b8df4
jjikky <kok5417@skuniv.ac.kr>
2024-06-19 13:24:40 +0900

    🔊 userSchema TODO

--- src/routes/user/user.model.js
+++ src/routes/user/user.model.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        createdAt: { type: Date, default: Date.now },
-        updatedAt: { type: Date, default: Date.now },
+        // TODO : 동작 확인 후 주석 삭제
+        // createdAt: { type: Date, default: Date.now },
+        // updatedAt: { type: Date, default: Date.now },
         deletedAt: { type: Date, default: null },
         nickname: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
