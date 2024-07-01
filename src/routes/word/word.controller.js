557a2362aa7e170f020ffc871e71f45f220c5b7a
jjikky <kok5417@skuniv.ac.kr>
2024-07-01 12:07:51 +0900

    ♻️ 검색 결과 조회 서비스 분리 및 대소문자 필터

--- src/routes/word/word.controller.js
+++ src/routes/word/word.controller.js
@@ -1,4 +1,5 @@
 const wordService = require('./word.service');
+const userService = require('../user/user.service');
 const sendResponse = require('../../common/utils/response-handler');
 const ErrorMessage = require('../../common/constants/error-message');
 const SucesssMessage = require('../../common/constants/success-message');
@@ -9,10 +10,18 @@ exports.getRankWords = async (req, res) => {};
 
 exports.getSearchWords = async (req, res) => {
     try {
-        const { _id } = req.user;
-        const validData = validateRequest(searchTermSchema, req.params); // 검색어 검증
-        const searchTerm = validData.searchTerm; // 요청 파라미터에서 검색어 추출
-        const data = await wordService.getSearchWords(_id, searchTerm); // 검색어 조회
+        const _id = req?.user ? req.user._id : null;
+
+        // 검색어 검증
+        const validData = validateRequest(searchTermSchema, req.params);
+        // 요청 파라미터에서 검색어 추출
+        const searchTerm = validData.searchTerm;
+        // 검색어 조회
+        const data = await wordService.getSearchWords(searchTerm);
+
+        if (_id) {
+            await userService.updateRecentSearch(_id, searchTerm);
+        }
 
         sendResponse.ok(res, {
             message: SucesssMessage.SEARCH_WORDS_SUCCESS,
