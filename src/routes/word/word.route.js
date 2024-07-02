59fe4581c25addfa3ed541dbf60521a9df39eefe
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-03 07:49:33 +0900

    ✨ 전체 단어 목록 경로 추가

--- src/routes/word/word.route.js
+++ src/routes/word/word.route.js
@@ -10,7 +10,7 @@ wordRouter.get('/rank', getRankWords); // 인기 검색어 조회
 // wordRouter.get('/:searchTerm', getRelatedWords); // params로 전달받은 검색어 searchTerm을 포함하는 단어 조회
 wordRouter.post('/search/:searchTerm', isUser, getSearchWords);
 
-// 전체 단어목록 조회
+// 전체 단어 목록 (쿼리 스트링에 정렬 & 페이지 정보)
 wordRouter.get('/word', getWords);
 
 module.exports = wordRouter;
