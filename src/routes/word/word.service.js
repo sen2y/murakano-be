27f1dd9fc06dbebea7f58433c4aec07ea825eb3f
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-03 07:49:07 +0900

    ✨ 단어 목록 조회 함수 추가

--- src/routes/word/word.service.js
+++ src/routes/word/word.service.js
@@ -11,3 +11,9 @@ exports.getRankWords = async () => {
     const rankWords = await wordRepository.getRankWords();
     return rankWords;
 };
+
+// 전체 단어 목록 조회 & 정렬 (최초 로딩시, 최신순 정렬)
+exports.findAllWords = async (sort = 'recent', page = 1, limit = 10) => {
+    const words = await wordRepository.findAllWords(sort, page, limit);
+    return words;
+};
