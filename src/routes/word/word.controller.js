1ba4977f44d392405109502ef5501138370f1c0c
sen2y <pse6704@sookmyung.ac.kr>
2024-07-06 15:41:17 +0900

    🐛  검색결과 api 복구

--- src/routes/word/word.controller.js
+++ src/routes/word/word.controller.js
@@ -1,9 +1,37 @@
 const wordService = require('./word.service');
+const userService = require('../user/user.service');
 const sendResponse = require('../../common/utils/response-handler');
 const ErrorMessage = require('../../common/constants/error-message');
 const SucesssMessage = require('../../common/constants/success-message');
 const { validateRequest } = require('../../common/utils/request.validator');
-const { searchTermSchema } = require('./word.schema');
+const { searchTermSchema, relatedTermSchema } = require('./word.schema');
+
+// 검색어 조회
+exports.getSearchWords = async (req, res) => {
+    try {
+        const _id = req?.user ? req.user._id : null;
+        // 검색어 검증
+        const validData = validateRequest(searchTermSchema, req.params);
+        // 요청 파라미터에서 검색어 추출
+        const searchTerm = validData.searchTerm;
+        // 검색어 조회
+        const data = await wordService.getSearchWords(searchTerm);
+        if (_id) {
+            await userService.updateRecentSearch(_id, searchTerm);
+        }
+        const message = data ? SucesssMessage.SEARCH_WORDS_SUCCESS : SucesssMessage.SEARCH_WORDS_NONE;
+        sendResponse.ok(res, {
+            message,
+            data,
+        });
+    } catch (error) {
+        console.log(error);
+        if (error?.type) {
+            return sendResponse.badRequest(res, error.message);
+        }
+        sendResponse.fail(req, res, ErrorMessage.SEARCH_WORDS_ERROR);
+    }
+};
 
 // 인기 검색어 조회
 exports.getRankWords = async (req, res) => {
@@ -22,7 +50,7 @@ exports.getRankWords = async (req, res) => {
 exports.getRelatedWords = async (req, res) => {
     try {
         let { keyword, limit } = req.query;
-        keyword = validateRequest(searchTermSchema, keyword);
+        keyword = validateRequest(relatedTermSchema, keyword);
         console.log(keyword, limit);
         const data = await wordService.getRelatedWords(keyword, limit);
         sendResponse.ok(res, {
